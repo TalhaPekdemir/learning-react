1390ae970bcdb7a010f9ee0abd6dd5e0c9e058b5
TalhaPekdemir <75481622+TalhaPekdemir@users.noreply.github.com>
2024-08-05 17:25:07 +0300

    adde details screen

--- v18/03-FoodRecipeApp/foodrecipeapp/src/App.jsx
+++ v18/03-FoodRecipeApp/foodrecipeapp/src/App.jsx
@@ -11,19 +11,20 @@ import "./App.css";
 
 function App() {
   const [foodData, setFoodData] = useState({});
-  const [foodId, setFoodId] = useState("");
+  const [foodId, setFoodId] = useState();
 
   return (
     <div className="App">
       <Navbar />
       <Search setFoodData={setFoodData} />
       <FlexContainer>
-        <ColumnContainer>
-          <FoodList foodList={foodData.results} setFoodId={setFoodId} />
-        </ColumnContainer>
-        <ColumnContainer>
-          <FoodDetails foodId={foodId} />
-        </ColumnContainer>
+        <FoodList foodList={foodData.results} setFoodId={setFoodId} />
+
+        {foodId ? (
+          <ColumnContainer>
+            <FoodDetails foodId={foodId} />
+          </ColumnContainer>
+        ) : null}
       </FlexContainer>
     </div>
   );
--- v18/03-FoodRecipeApp/foodrecipeapp/src/components/FoodDetails.jsx
+++ v18/03-FoodRecipeApp/foodrecipeapp/src/components/FoodDetails.jsx
@@ -1,3 +1,44 @@
+import { useEffect, useState } from "react";
+import { RECIPES_URL } from "../../utils/consts/spoonacularapi-endpoints";
+
+import styles from "./fooddetails.module.css";
+
 export default function FoodDetails({ foodId }) {
-  return <div>FoodDetails: {foodId}</div>;
+  const [information, setInformation] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (foodId) {
+      fetch(`${RECIPES_URL}/${foodId}/information`, {
+        method: "GET",
+        headers: { "x-api-key": import.meta.env.VITE_SPOONACULAR_API_KEY },
+      })
+        .then((res) => res.json())
+        .then((data) => setInformation(data) + setIsLoading(false))
+        .catch((error) => console.error(error));
+    }
+  }, [foodId]);
+
+  return (
+    <div className={styles.infoCard}>
+      <div>
+        <h1 className={styles.infoText}>{information.title}</h1>
+        <img
+          className={styles.infoImage}
+          src={information.image}
+          alt={information.title}
+        />
+      </div>
+      {isLoading ? null : (
+        <div className={styles.instructions}>
+          <h2>Instructions:</h2>
+          <ol>
+            {information?.analyzedInstructions[0].steps?.map((item) => (
+              <li>{item.step}</li>
+            ))}
+          </ol>
+        </div>
+      )}
+    </div>
+  );
 }
