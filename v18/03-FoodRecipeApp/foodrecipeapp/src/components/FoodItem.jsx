e33f5fa9bf75ba1a045b94be40d9dda6aa55f1c0
TalhaPekdemir <75481622+TalhaPekdemir@users.noreply.github.com>
2024-08-08 17:39:17 +0300

    change design

--- v18/03-FoodRecipeApp/foodrecipeapp/src/components/FoodItem.jsx
+++ v18/03-FoodRecipeApp/foodrecipeapp/src/components/FoodItem.jsx
@@ -4,17 +4,12 @@ export default function FoodItem({ food, setFoodId }) {
   return (
     <div className={styles.itemCard}>
       <img className={styles.cardImage} src={food.image} alt={food.title} />
-      <div className={styles.cardContent}>
-        <h3 className={styles.cardText}>{food.title}</h3>
-      </div>
-      <div className={styles.cardButtonContainer}>
-        <button
-          className={styles.cardButton}
-          onClick={() => setFoodId(food.id)}
-        >
-          Show Recipe
-        </button>
-      </div>
+
+      <h3 className={styles.cardText}>{food.title}</h3>
+
+      <button className={styles.cardButton} onClick={() => setFoodId(food.id)}>
+        Show Recipe
+      </button>
     </div>
   );
 }
