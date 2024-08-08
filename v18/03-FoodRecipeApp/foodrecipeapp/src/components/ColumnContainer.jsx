6d42ba081de9afe42fde368b0109eaee28d089ec
TalhaPekdemir <75481622+TalhaPekdemir@users.noreply.github.com>
2024-08-08 17:38:36 +0300

    remove breaking flex col

--- v18/03-FoodRecipeApp/foodrecipeapp/src/components/ColumnContainer.jsx
+++ v18/03-FoodRecipeApp/foodrecipeapp/src/components/ColumnContainer.jsx
@@ -1,5 +1,6 @@
 import styles from "./columncontainer.module.css";
 
+// TEST
 export default function ColumnContainer({ children }) {
   return <div className={styles.columnContainer}>{children}</div>;
 }
